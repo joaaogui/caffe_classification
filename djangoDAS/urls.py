06d548db864f232c510366593a2f51f1334b93ab
João Guilherme <joaaogui@gmail.com>
2016-11-18 19:28:47 -0200

    Face detection from tutorial(5 minutes) working

--- djangoDAS/urls.py
+++ djangoDAS/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from face_detector import views
 
 urlpatterns = [
+    url(r'^face_detection/detect/$', views.detect, name='detect'),
     url(r'^admin/', admin.site.urls),
 ]
